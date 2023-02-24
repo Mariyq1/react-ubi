97354fa2fde72e27a125290f6a37dd0aa6a76395
Mariyq1 <m4697597@gmail.com>
2023-02-24 20:00:11 +0100

    Create counter for class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ClassCounter from "./Components/ClassCounter";
 import Counter from "./Components/Counter";
 
 function App() {
   return (
   <div className="App">
-    <Counter/>
+    <ClassCounter/>
     </div>
   );
 }
