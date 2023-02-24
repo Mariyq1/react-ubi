be232314b2bd4511ba6563e7fc516c1784e61c93
Mariyq1 <m4697597@gmail.com>
2023-02-24 19:41:54 +0100

    remove counter for likes to Counter,jsx

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import Counter from "./Components/Counter";
 
 function App() {
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState("Enter text");
-  function increment (){
-    setLikes(likes +1)
-  }
-  function decrement (){
-    setLikes(likes -1)
-  }
   return (
-    <div className="App">
-      <h1>{likes}</h1>
-      <h2>{value}</h2>
-      <input type="text" 
-      value={value}
-      onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={increment}>
-        Increment
-      </button>
-      <button onClick={decrement}>
-        Decrement
-      </button>
-      
+  <div className="App">
+    <Counter/>
     </div>
   );
 }
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,3 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Counter = 
+const Counter = function () {
+    const [count, setCount] = useState(0);
+    function increment (){
+        setCount(count +1)
+    }
+    function decrement (){
+        setCount(count -1)
+    }
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+export default Counter;
