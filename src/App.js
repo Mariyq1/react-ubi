619ba04d60321221817ca392cb9c041f9a8e9c3c
Mariyq1 <m4697597@gmail.com>
2023-02-25 18:38:07 +0100

    create new component for our form, using callback

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import './styles/App.css';
 import PostList from "./Components/PostList";
-import MyButton from "./Components/UI/button/MyButton";
-import MyInput from "./Components/UI/button/input/MyInput";
+import PostForm from "./Components/PostForm";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -10,30 +9,12 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ])
-  const [post, setPost] = useState({title: '', body: ''});
-  const addNewPost = (e) =>{
-    e.preventDefault();
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: '', body: ''});
+   const createPost = (newPost)=>{
+    setPosts([...posts, newPost])
   }
-  
   return (
     <div className="App">
-      <form>
-        <MyInput 
-          type="text" 
-          placeholder="Name of the post"
-          value={post.title}
-          onChange={e=> setPost({...post, title: e.target.value})}
-          />
-        <MyInput 
-          type="text" 
-          placeholder="Post description"
-          value={post.body}
-          onChange={e=> setPost({...post, body:e.target.value})}
-          />
-        <MyButton onClick={addNewPost}>Create post</MyButton>
-      </form>
+      <PostForm create={createPost}/>
       <PostList posts={posts} title="Posts about JS"/>
     </div>
     );
