626c14af5666789c2a153d4f4d0fde6b7316b22f
Mariyq1 <m4697597@gmail.com>
2023-02-25 16:03:24 +0100

    Create and styling button component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './styles/App.css';
 import PostList from "./Components/PostList";
+import MyButton from "./Components/UI/button/MyButton";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -8,15 +9,14 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ])
-  const [posts2, setPosts2] = useState([
-    {id: 1, title: 'Python', body: 'Description'},
-    {id: 2, title: 'Python 2', body: 'Description'},
-    {id: 3, title: 'Python 3', body: 'Description'}
-  ])
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Name of the post"/>
+        <input type="text" placeholder="Post description"/>
+        <MyButton>Create post</MyButton>
+      </form>
       <PostList posts={posts} title="Posts about JS"/>
-      <PostList posts={posts2} title="Posts about Python"/>
     </div>
     );
 }
