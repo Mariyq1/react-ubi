05fd576ac341e8a777b985ee52a14400785e158a
Mariyq1 <m4697597@gmail.com>
2023-02-25 15:39:04 +0100

    Create list for posts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/App.css';
-import PostItem from "./Components/PostItem";
+import PostList from "./Components/PostList";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'}
+  ])
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Python 2', body: 'Description'},
+    {id: 3, title: 'Python 3', body: 'Description'}
+  ])
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>
+      <PostList posts={posts} title="Posts about JS"/>
+      <PostList posts={posts2} title="Posts about Python"/>
     </div>
     );
 }
