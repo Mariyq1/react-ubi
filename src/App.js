cdef902e5984f1bf57cfe9379713d48d39fc1c6a
Mariyq1 <m4697597@gmail.com>
2023-02-25 16:29:21 +0100

    Create function to our button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './styles/App.css';
 import PostList from "./Components/PostList";
 import MyButton from "./Components/UI/button/MyButton";
+import MyInput from "./Components/UI/button/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,12 +10,22 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ])
+  const [title, setTitle] = useState("");
+  const addNewPost = (e)=>{
+    e.preventDefault();
+    console.log(title);
+  }
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Name of the post"/>
-        <input type="text" placeholder="Post description"/>
-        <MyButton>Create post</MyButton>
+        <MyInput 
+          type="text" 
+          placeholder="Name of the post"
+          value={title}
+          onChange={e=> setTitle(e.target.value)}
+          />
+        <MyInput type="text" placeholder="Post description"/>
+        <MyButton onClick={addNewPost}>Create post</MyButton>
       </form>
       <PostList posts={posts} title="Posts about JS"/>
     </div>
